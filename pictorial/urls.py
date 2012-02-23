08adf433d35239be6e899d00ade365ba5cf80e15
Faheem Patel <faheem@faheempatel.com>
2012-02-23 21:39:53 +0000

    Added view and url for Index

--- pictorial/urls.py
+++ pictorial/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from pictorial.views import AboutView
+from pictorial.views import IndexView, AboutView
 
 admin.autodiscover()
 
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pictorial.views.home', name='home'),
     # url(r'^pictorial/', include('pictorial.foo.urls')),
+    (r'', IndexView.as_view()),
     (r'^about/', AboutView.as_view()),
 
     # Django admin site URL's
--- pictorial/views.py
+++ pictorial/views.py
@@ -2,6 +2,8 @@
 
 from django.views.generic import TemplateView
 
+class IndexView(TemplateView):
+    template_name = "index.html"
+
 class AboutView(TemplateView):
     template_name = "about.html"
-
